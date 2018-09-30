ba12cb0b9992c5dfafd57be336f2430fa0359e15
adriamb <>
2018-09-30 16:38:13 +0200

    Added unstructured storage. Removed idiomatic internals

--- contracts/lib/DelegateProxySlotStorage.sol
+++ contracts/lib/DelegateProxySlotStorage.sol
@@ -1,41 +1,36 @@
 pragma solidity ^0.4.24;
 
+import './UnstructuredStorage.sol';
+
 contract DelegateProxySlotStorage {
-    bytes32 constant private IMPL_SLOT = keccak256("PROXY_IMPL_SLOT");
-    bytes32 constant private RECOVERER_SLOT = keccak256("PROXY_RECOVERER_SLOT");
-    bytes32 constant private PROPOSED_SLOT = keccak256("PROXY_PROPOSEDRECOVERER_SLOT");
 
-    function __getProxyInfo() internal view returns (address impl, address recoverer, address proposed) {
+    using UnstructuredStorage for bytes32;
+
+    // IMPL_SLOT=keccak256("iden3.core.impl.slot")
+    bytes32 constant private IMPL_SLOT = 0x1233c892137aa32e0f1c4b5359db9038b88dde2eb3927c5ee83dc8c9b3e17c0f;
+    // RECOVERER_SLOT=keccak256("iden3.core.recoverer.slot")
+    bytes32 constant private RECOVERER_SLOT = 0x44f094e9bb0645dc669d08e0f0767e604381cfc211e15a1dbae98fd3b535f285;
+    // PROPOSED_SLOT=keccak256("iden3.core.recovererprop.slot")
+    bytes32 constant private PROPOSED_SLOT = 0x9e67005d2760c1e41a2ea81ffa69f265791c0630cb5295c10534812389883dc4;
+
+    function getProxyInfo() internal view returns (address impl, address recoverer, address proposed) {
         
-        bytes32 implslot = IMPL_SLOT;
-        bytes32 recovererlot = RECOVERER_SLOT;
-        bytes32 proposedslot = PROPOSED_SLOT;
-
-        assembly {
-            impl := sload(implslot)
-            recoverer := sload(recovererlot)
-            proposed := sload(proposedslot)
-        }
+        return (
+            IMPL_SLOT.getStorageAddress(),
+            RECOVERER_SLOT.getStorageAddress(),
+            PROPOSED_SLOT.getStorageAddress()
+        );
     }
 
-    function __setProxyImpl(address _address) internal {
-        bytes32 slot = IMPL_SLOT;
-        assembly {
-            sstore(slot,_address)
-        }
+    function setProxyImpl(address _address) internal {
+        IMPL_SLOT.setStorageAddress(_address);
     }
 
-    function __setProxyRecoverer(address _address) internal {
-        bytes32 slot = RECOVERER_SLOT;
-        assembly {
-            sstore(slot,_address)
-        }
+    function setProxyRecoverer(address _address) internal {
+        RECOVERER_SLOT.setStorageAddress(_address);
     }
 
-    function __setProxyRecovererProp(address _address) internal {
-        bytes32 slot = PROPOSED_SLOT;
-        assembly {
-            sstore(slot,_address)
-        }
+    function setProxyRecovererProp(address _address) internal {
+        PROPOSED_SLOT.setStorageAddress(_address);
     }
 }
---
+++ contracts/lib/UnstructuredStorage.sol
@@ -0,0 +1,44 @@
+/* 
+  taken from
+  https://github.com/aragon/aragonOS/blob/a56234e3c0265878d5f105c6be2b81f2108f57d6/contracts/common/UnstructuredStorage.sol
+*/
+
+/*
+ * SPDX-License-Identitifer:    MIT
+ */
+
+pragma solidity ^0.4.24;
+
+library UnstructuredStorage {
+    function getStorageBool(bytes32 position) internal view returns (bool data) {
+        assembly { data := sload(position) }
+    }
+
+    function getStorageAddress(bytes32 position) internal view returns (address data) {
+        assembly { data := sload(position) }
+    }
+
+    function getStorageBytes32(bytes32 position) internal view returns (bytes32 data) {
+        assembly { data := sload(position) }
+    }
+
+    function getStorageUint256(bytes32 position) internal view returns (uint256 data) {
+        assembly { data := sload(position) }
+    }
+
+    function setStorageBool(bytes32 position, bool data) internal {
+        assembly { sstore(position, data) }
+    }
+
+    function setStorageAddress(bytes32 position, address data) internal {
+        assembly { sstore(position, data) }
+    }
+
+    function setStorageBytes32(bytes32 position, bytes32 data) internal {
+        assembly { sstore(position, data) }
+    }
+
+    function setStorageUint256(bytes32 position, uint256 data) internal {
+        assembly { sstore(position, data) }
+    }
+}
