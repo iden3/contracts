07ba9f64b203d5a69b2d58fc523c0c4387d2d56a
adriamb <>
2018-10-02 12:04:15 +0200

    added tests

--- test/helpers/assertFail.js
+++ test/helpers/assertFail.js
@@ -1,11 +1,16 @@
 /* global assert */
 
-module.exports = async (callback) => {
+module.exports = async (callback, reason) => {
     try {
         await callback;
     } catch (error) {
-        if (error.message.search("VM Exception while processing transaction") !== -1) return;
-        throw error;
+        if (error.message.search("VM Exception while processing transaction") === -1) {
+            throw error;
+        }
+        if (typeof reason !== 'undefined' && error.message.search("Reason given: "+reason+".") === -1) {
+            throw error;
+        }
+        return;
     }
     assert(false, "Transaction should fail");
 };
