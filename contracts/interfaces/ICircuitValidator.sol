5c6b6889f8800a9258d0eb803b2361737b65dec8
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-07-12 19:13:57 +0300

    add interfaces

---
+++ contracts/interfaces/ICircuitValidator.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+interface ICircuitValidator {
+    function verify(
+        uint256[] memory inputs,
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c
+    ) external view returns (bool r);
+    function getCircuitId() external pure returns (string memory r);
+}
+
---
+++ contracts/interfaces/IZKPAirdrop.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+interface IZKPAirdrop {
+    function mintWithProof(
+        uint256[] memory inputs,
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c)
+    external returns (bool);
+}
