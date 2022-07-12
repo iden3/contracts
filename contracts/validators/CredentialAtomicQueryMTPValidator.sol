ca2275e8ff2afb85f4d6525fe58d8d060cfa3210
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-07-12 19:15:20 +0300

    rename to validator

--- contracts/validators/CredentialAtomicQueryMTPValidator.sol
+++ contracts/validators/CredentialAtomicQueryMTPValidator.sol
@@ -2,34 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "./lib/GenesisUtils.sol";
+import "../lib/GenesisUtils.sol";
+import "../interfaces/ICircuitValidator.sol";
+import "../interfaces/IVerifier.sol";
+import "../interfaces/IState.sol";
 
-interface IVerifier {
-    function verifyProof(
-        uint256[2] memory a,
-        uint256[2][2] memory b,
-        uint256[2] memory c,
-        uint256[74] memory input
-    ) external view returns (bool r);
-}
 
-interface IState {
-    function getState(uint256 id) external view returns (uint256);
 
-    function getTransitionInfo(uint256 state)
-        external
-        view
-        returns (
-            uint256,
-            uint256,
-            uint64,
-            uint64,
-            uint256,
-            uint256
-        );
-}
 
-contract CredentialAtomicQueryMTP is OwnableUpgradeable {
+
+contract CredentialAtomicQueryMTPValidator is OwnableUpgradeable , ICircuitValidator{
+
+    string constant CIRCUIT_ID = "credentialAtomicQueryMTP";
     IVerifier public verifier;
     IState public state;
 
@@ -51,23 +35,28 @@ contract CredentialAtomicQueryMTP is OwnableUpgradeable {
     {
         revocationStateExpirationTime = expirationTime;
     }
+    function getCircuitId() external pure returns (string memory id) {
+        return CIRCUIT_ID;
+    }
 
     function verify(
-        uint256[74] memory inputs,
+        uint256[] memory inputs,
         uint256[2] memory a,
         uint256[2][2] memory b,
         uint256[2] memory c
     ) public view returns (bool r) {
+
+        // verify that zkp is valid
+        require(
+            verifier.verifyProof(a, b, c, inputs),
+            "MTP is not valid"
+        );
+
         uint256 userId = inputs[0];
         uint256 userState = inputs[1];
         uint256 issuerClaimIdenState = inputs[3];
         uint256 issuerId = inputs[4];
         uint256 issuerClaimNonRevState = inputs[5];
-
-        require(
-            verifier.verifyProof(a, b, c, inputs),
-            "MTP Proof could not be verified"
-        );
 
         // 1. User state must be latest or genesis
 
