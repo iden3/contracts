7609c85346a2bc5cf6b4ce99cd4feca3d9c3a91d
vmidyllic <74898029+vmidyllic@users.noreply.github.com>
2022-07-13 15:05:05 +0300

    move query to Validator interface

--- contracts/interfaces/IERC20zkp.sol
+++ contracts/interfaces/IERC20zkp.sol
@@ -1,13 +1,6 @@
 pragma solidity ^0.8.0;
 
 interface IERC20ZKP {
-    struct CircuitQuery {
-        uint256 schema;
-        uint256 slotIndex;
-        uint256 operator;
-        uint256[] value;
-        string circuitId;
-    }
 
     function transferWithProof(
         uint256[] memory inputs,
@@ -17,5 +10,4 @@ interface IERC20ZKP {
         uint256 amount
     ) external returns (bool);
 
-    function getCircuitQuery() external view returns (CircuitQuery memory);
 }
