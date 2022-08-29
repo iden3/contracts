be5dad2c7e3bef393633b5f9b41a0541a3b77e6b
Andriian Chestnykh <andriian@snowball.money>
2022-08-29 12:19:20 +0300

    IDEN-446 Fix the script for migration to State V2

--- scripts/smt-migration.ts
+++ scripts/smt-migration.ts
@@ -1,14 +1,19 @@
 import { SmtStateMigration } from "./smt-state-migration";
+import { deployPoseidons } from "../test/deploy-utils";
+const { ethers } = require("hardhat");
 
 async function main() {
   /* to run script:
   npx hardhat run --network mumbai scripts/smt-deploy.ts
   */
+  const [owner] = await ethers.getSigners();
+  const { poseidon2Elements, poseidon3Elements } = await deployPoseidons(owner);
+
   await new SmtStateMigration(true).run(
-    "<state address>",
-    "<poseidon2 address>",
-    "<poseidon3 address>",
-    27573663, // start block
+    "<state address>", // put your State smart contract address here !!!
+    poseidon2Elements.address,
+    poseidon3Elements.address,
+    0, // put your block here. Normally, this should be the block of the previous State contract version deployment
     3500 // blocks chunk size
   );
 }
