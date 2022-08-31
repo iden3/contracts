d7a4ce70a3859d550bd6712e93725dc3299fbc34
Andriian Chestnykh <andriian@snowball.money>
2022-08-31 14:36:30 +0300

    IDEN-446 Replace TODO with a comment

--- test/smt/utils.ts
+++ test/smt/utils.ts
@@ -16,7 +16,7 @@ export type MtpProof = {
   siblings: FixedArray<string, 32>;
   oldKey: number;
   oldValue: number;
-  isOld0: false; // todo research why this is always false in go-merkle-sql
+  isOld0: false; // for mtp proofs verification in circom isOld0 is always false
   key: number;
   value: number;
   fnc: 0 | 1;
