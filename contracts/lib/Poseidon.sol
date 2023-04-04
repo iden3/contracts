c1a249c6221176faea8c8fbfba3f42a53a72a907
Dimasik Kolezhniuk <dmytro.kolezhniuk@idyllicvision.com>
2023-04-04 20:05:30 +0200

    Sponge hash fix (#116)
    * Sponge hash fix

--- contracts/lib/Poseidon.sol
+++ contracts/lib/Poseidon.sol
@@ -26,58 +26,30 @@ library PoseidonUnit6L {
 }
 
 library SpongePoseidon {
-    uint32 constant BATCH_SIZE = 5;
-    uint32 constant HASH_FN_BATCH_SIZE = 6;
-
-    function getValueByIndex(
-        uint256[] calldata arr,
-        uint32 idx,
-        uint32 length
-    ) internal pure returns (uint256) {
-        if (idx < length) {
-            return arr[idx];
-        }
-        return 0;
-    }
+    uint32 constant BATCH_SIZE = 6;
 
     function hash(uint256[] calldata values) public pure returns (uint256) {
-        uint32 iterationCount = 0;
-        uint32 length = uint32(values.length);
-        uint256 fullHash = PoseidonUnit6L.poseidon(
-            [
-                getValueByIndex(values, 0, length),
-                getValueByIndex(values, 1, length),
-                getValueByIndex(values, 2, length),
-                getValueByIndex(values, 3, length),
-                getValueByIndex(values, 4, length),
-                getValueByIndex(values, 5, length)
-            ]
-        );
-
-        uint32 restLength = length - HASH_FN_BATCH_SIZE;
-        if (restLength > BATCH_SIZE) {
-            uint32 r = restLength % BATCH_SIZE;
-            uint32 diff = 0;
-            if (r != 0) {
-                diff = BATCH_SIZE - r;
+        uint256[BATCH_SIZE] memory frame = [uint256(0), 0, 0, 0, 0, 0];
+        bool dirty = false;
+        uint256 fullHash = 0;
+        uint32 k = 0;
+        for (uint32 i = 0; i < values.length; i++) {
+            dirty = true;
+            frame[k] = values[i];
+            if (k == BATCH_SIZE - 1) {
+                fullHash = PoseidonUnit6L.poseidon(frame);
+                dirty = false;
+                frame = [uint256(0), 0, 0, 0, 0, 0];
+                frame[0] = fullHash;
+                k = 1;
+            } else {
+                k++;
             }
-            iterationCount = (restLength + diff) / BATCH_SIZE;
         }
-
-        for (uint32 i = 0; i < iterationCount; i++) {
-            uint32 elemIdx = i * BATCH_SIZE + HASH_FN_BATCH_SIZE;
-            fullHash = PoseidonUnit6L.poseidon(
-                [
-                    fullHash,
-                    getValueByIndex(values, elemIdx, length),
-                    getValueByIndex(values, elemIdx + 1, length),
-                    getValueByIndex(values, elemIdx + 2, length),
-                    getValueByIndex(values, elemIdx + 3, length),
-                    getValueByIndex(values, elemIdx + 4, length)
-                ]
-            );
+        if (dirty) {
+            // we haven't hashed something in the main sponge loop and need to do hash here
+            fullHash = PoseidonUnit6L.poseidon(frame);
         }
-
         return fullHash;
     }
 }
