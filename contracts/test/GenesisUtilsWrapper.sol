b61948b26ac84a8dd7f48a0cf389d5e4e9dd8e8e
vbasiuk <volodymyr.basiuck@gmail.com>
2023-04-18 11:40:17 +0300

    fix solhint errors

--- contracts/test/GenesisUtilsWrapper.sol
+++ contracts/test/GenesisUtilsWrapper.sol
@@ -1,13 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity 0.8.16;
 
-
 import "../lib/GenesisUtils.sol";
 
 contract GenesisUtilsWrapper {
-
-     function isGenesisState(uint256 id, uint256 idState) public pure returns (bool) {
-       return GenesisUtils.isGenesisState(id, idState);
+    function isGenesisState(uint256 id, uint256 idState) public pure returns (bool) {
+        return GenesisUtils.isGenesisState(id, idState);
     }
-
 }
