e7ddc28fbf27bd2df1ea02badcc0bfa3e78e9a74
vbasiuk <volodymyr.basiuck@gmail.com>
2023-05-10 18:23:47 +0300

    fix linter

--- contracts/test/ClaimBuilderWrapper.sol
+++ contracts/test/ClaimBuilderWrapper.sol
@@ -5,7 +5,7 @@ import "../lib/ClaimBuilder.sol";
 
 contract ClaimBuilderWrapper {
     using ClaimBuilder for ClaimBuilder.ClaimData;
-    
+
     function buildClaim(ClaimBuilder.ClaimData calldata c) public pure returns (uint256[8] memory) {
         return ClaimBuilder.build(c);
     }
