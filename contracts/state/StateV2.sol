00008c7f5dbeeea46eea18a7ed57419e075884ff
vbasiuk <volodymyr.basiuck@gmail.com>
2023-06-20 08:23:02 +0300

    cleanup state contract

--- contracts/state/StateV2.sol
+++ contracts/state/StateV2.sol
@@ -129,10 +129,6 @@ contract StateV2 is Ownable2StepUpgradeable, IState {
             "Zero-knowledge proof of state transition is not valid"
         );
 
-        require(isOldStateGenesis == GenesisUtils.isGenesisState(id, oldState));
-        if (isOldStateGenesis) {
-            require(id == GenesisUtils.calcIdFromGenesisState(0x0212, oldState));
-        }
         _transitState(id, oldState, newState, isOldStateGenesis);
     }
 
