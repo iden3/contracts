7ae9b9bb6240ade7feba871480a6903011617efa
vbasiuk <volodymyr.basiuck@gmail.com>
2023-07-03 12:40:16 +0300

    add generic state-upgrade

--- scripts/upgrade/state/deploy-from-commit.sh
+++ scripts/upgrade/state/deploy-from-commit.sh
@@ -19,6 +19,7 @@ git checkout $currBranch
 npx hardhat compile
 cp test/upgrade/state-upgrade-template.ts test/upgrade/state-upgrade-$1.ts
 sed -i '' "s/.skip//gi" test/upgrade/state-upgrade-$1.ts  
-sed -i '' "s/{commit_hash}/$1/gi" test/upgrade/state-upgrade-$1.ts  
+sed -i '' "s/{commit_hash}/$1/gi" test/upgrade/state-upgrade-$1.ts
 sed -i '' "s/'contract_address_placeholder'/$contract_addr/gi" test/upgrade/state-upgrade-$1.ts
 npx hardhat test test/upgrade/state-upgrade-$1.ts --network localhost
+sed -i '' "s/{commit_hash}/$1/gi" scripts/upgrade/state/state-upgrade.ts 
