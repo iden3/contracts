a373450a9f10da79af5f75886db5fb46610f697f
Ilya <officalbatya@gmail.com>
2024-01-18 18:16:39 +0200

    add documentation

--- contracts/lib/OnchainNonMerklizedIdentityBase.sol
+++ contracts/lib/OnchainNonMerklizedIdentityBase.sol
@@ -5,15 +5,25 @@ import {IdentityLib} from "../lib/IdentityLib.sol";
 import {IdentityBase} from "../lib/IdentityBase.sol";
 import {Strings} from '@openzeppelin/contracts/utils/Strings.sol';
 
+/**
+ * @dev Contract building onchain non-merklized identity
+ * that can be transformed to W3C verifiable credential
+ */
 abstract contract OnchainNonMerklizedIdentityBase is IdentityBase  {
     using IdentityLib for IdentityLib.Data;
 
+    /**
+     * @dev CredentialInformation keep information about the credential
+     */
     struct CredentialInformation {
         string[] jsonLDContextUrls;
         string jsonSchemaUrl;
         string _type;
     }
 
+    /**
+     * @dev CredentialMetadata keep metadata about the credential
+     */
     struct CredentialMetadata {
         uint256 id;
         uint64 revocationNonce;
@@ -21,29 +31,48 @@ abstract contract OnchainNonMerklizedIdentityBase is IdentityBase  {
         uint64 expirationDate;
     }
 
+    /**
+     * @dev Claim keep information about claim and calculated hashIndex and hashValue
+     */
     struct Claim {
         uint256[8] coreClaim;
         uint256 hashIndex;
         uint256 hashValue;
     }
 
+    /**
+     * @dev SubjectField credential subject for the credential
+     * key - name of the field
+     * value - value of the field
+     * rawValue - raw value of the field, 
+     * is used for string and double types to restore source value in W3C verifiable credential
+     */
     struct SubjectField {
         string key;
         uint256 value;
         bytes rawValue;
     }
 
+    /**
+     * @dev Status information about the credential status
+     */
     struct Status {
         string id;
         string _type;
         uint64 revocationNonce;
     }
 
+    /**
+     * @dev Schema information about the credential schema
+     */
     struct Schema {
         string id;
         string _type;
     }
 
+    /**
+     * @dev IssuerState issuer state and roots
+     */
     struct IssuerState {
         uint256 rootOfRoots;
         uint256 claimsTreeRoot;
@@ -51,11 +80,17 @@ abstract contract OnchainNonMerklizedIdentityBase is IdentityBase  {
         uint256 value;
     }
 
+    /**
+     * @dev IssuerData issuer data
+     */
     struct IssuerData {
         uint256 id;
         IssuerState state;
     }
 
+    /**
+     * @dev IssuanceProof information about the issuance proof
+     */
     struct IssuanceProof {
         string _type;
         uint256[8] coreClaim;
@@ -63,6 +98,10 @@ abstract contract OnchainNonMerklizedIdentityBase is IdentityBase  {
         SmtLib.Proof mtp;
     }
 
+    /**
+     * @dev CredentialData information about the credential.
+     * Can be transformed to W3C verifiable credential
+     */
     struct CredentialData {
         string id;
         string[] context;
@@ -76,6 +115,9 @@ abstract contract OnchainNonMerklizedIdentityBase is IdentityBase  {
         IssuanceProof[] proof;
     }
 
+    /**
+     * @dev Id
+     */
     struct Id {
         uint256 id;
     }
@@ -84,13 +126,23 @@ abstract contract OnchainNonMerklizedIdentityBase is IdentityBase  {
         IdentityBase.initialize(_stateContractAddr);
     }
 
+    /**
+     * @dev listUserCredentials. Get list of user credentials identifiers
+     * @param _userId user id
+     */
     function listUserCredentials(uint256 _userId) external virtual view returns (Id[] memory);
 
+    /**
+     * @dev getCredential. Get credential by user id and credential id
+     */
     function getCredential(
         uint256 _userId,
         uint256 _credentialId
     ) external virtual view returns (CredentialData memory);
 
+    /**
+     * @dev processOnchainCredentialData. Build credential data from core claim and additional information
+     */
     function processOnchainCredentialData(
         CredentialInformation memory credentialInformation,
         CredentialMetadata memory credentialMetadata,
