5bf8aeb8656db257f54034bcea1bff6ca75c1e2e
Andriian Chestnykh <>
2024-01-30 12:56:11 +0000

    Bump compiler pragma version

--- contracts/verifiers/UniversalVerifier.sol
+++ contracts/verifiers/UniversalVerifier.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity 0.8.16;
 
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/IERC165.sol";
