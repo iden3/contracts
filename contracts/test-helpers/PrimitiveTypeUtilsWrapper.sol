450a16f01bc088a33d9b1ea24ea94d490c26fbf2
GopherDID <74898029+vmidyllic@users.noreply.github.com>
2024-03-09 20:55:13 +0200

    fix rename primitive methods (#218)
    * rework primitive methods

---
+++ contracts/test-helpers/PrimitiveTypeUtilsWrapper.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+import {PrimitiveTypeUtils} from "../lib/PrimitiveTypeUtils.sol";
+import {GenesisUtils} from "../lib/GenesisUtils.sol";
+
+contract PrimitiveTypeUtilsWrapper {
+    function addressToUint256LE(address _addr) public pure returns (uint256) {
+        return PrimitiveTypeUtils.addressToUint256LE(_addr);
+    }
+
+    function uint256LEToAddress(uint256 input) public pure returns (address) {
+        return PrimitiveTypeUtils.uint256LEToAddress(input);
+    }
+
+    function addressToUint256(address _addr) public pure returns (uint256) {
+        return PrimitiveTypeUtils.addressToUint256(_addr);
+    }
+
+    function uint256ToAddress(uint256 input) public pure returns (address) {
+        return PrimitiveTypeUtils.uint256ToAddress(input);
+    }
+}
