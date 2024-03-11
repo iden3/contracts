a59b9b53011f2ffb9170f5408b1b174d38a1ede5
Ilya <officalbatya@gmail.com>
2024-03-11 18:09:33 +0200

    add IIdentifiable inteface (#220)

---
+++ contracts/interfaces/IIdentifiable.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+/**
+ * @dev IIdentifiable. Interface for identifiable entities.
+ */
+interface IIdentifiable {
+    /**
+     * @dev getId. Get id of the identity
+     */
+    function getId() external view returns (uint256);
+}
