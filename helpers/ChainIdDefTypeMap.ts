2d38d39b65ce3d254f9c26f34c3f6010b19382a6
GopherDID <74898029+vmidyllic@users.noreply.github.com>
2024-04-01 14:49:37 +0300

    Feature/amoy (#228)
    * deploy contracts to amoy

--- helpers/ChainIdDefTypeMap.ts
+++ helpers/ChainIdDefTypeMap.ts
@@ -1,7 +1,7 @@
 export const chainIdDefaultIdTypeMap = new Map()
-    .set(31337, '0x0212') // hardhat
-    .set(80001, '0x0212') // polygon mumbai
-    .set(1101,  '0x0231') // zkEVM
-    .set(1442,  '0x0232') // zkEVM testnet
-    .set(137,   '0x0211'); // polygon main 
-    
+  .set(31337, "0x0212") // hardhat
+  .set(1101, "0x0231") // zkEVM
+  .set(1442, "0x0232") // zkEVM testnet
+  .set(137, "0x0211") // polygon main
+  .set(80001, "0x0212") // polygon mumbai
+  .set(80002, "0x0213"); // polygon amoy
