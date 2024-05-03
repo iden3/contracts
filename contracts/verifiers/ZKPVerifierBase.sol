bb0e000201168d9f5feb80148f101f573e28c845
Andriian Chestnykh <>
2024-05-03 09:38:16 +0100

    Fix pragma

--- contracts/verifiers/ZKPVerifierBase.sol
+++ contracts/verifiers/ZKPVerifierBase.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity 0.8.20;
 
 import {IZKPVerifier} from "../interfaces/IZKPVerifier.sol";
 import {ICircuitValidator} from "../interfaces/ICircuitValidator.sol";
