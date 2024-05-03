ad8f49fbe29c7de6a59543ae1f1a2db6d6095e87
Andriian Chestnykh <>
2024-05-03 13:34:44 +0100

    Bring back ZKPVerifier and a bump a major version

---
+++ contracts/verifiers/ZKPVerifier.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {EmbeddedZKPVerifier} from "./EmbeddedZKPVerifier.sol";
+
+/**
+ * @dev The ZKPVerifier is deprecated and will be removed in the future major versions
+ * Please use EmbeddedZKPVerifier instead
+ */
+contract ZKPVerifier is EmbeddedZKPVerifier {
+}
