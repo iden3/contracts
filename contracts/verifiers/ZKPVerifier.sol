1f24e9c2c6429b766e671ed3079357a1cd5c261a
Andriian Chestnykh <>
2024-05-17 10:59:31 +0100

    Revert "Remove ZKPVerifier"
    This reverts commit 1e7a3d0672a02eae58bf4e1daeca0711c54cde16.

---
+++ contracts/verifiers/ZKPVerifier.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+import {EmbeddedZKPVerifier} from "./EmbeddedZKPVerifier.sol";
+
+/**
+ * @dev The ZKPVerifier is deprecated and will be removed in the future major versions
+ * Please use EmbeddedZKPVerifier instead
+ */
+contract ZKPVerifier is EmbeddedZKPVerifier {}
