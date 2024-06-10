4053cc134c1f4eee291a404e1c1141556ea4b246
vbasiuk <volodymyr.basiuck@gmail.com>
2024-06-10 10:47:16 +0300

    fix linter

--- contracts/imports.sol
+++ contracts/imports.sol
@@ -3,5 +3,5 @@ pragma solidity 0.8.20;
 
 // We import these here to force Hardhat to compile them.
 // This ensures that their artifacts are available for Hardhat Ignition to use.
-import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
-import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import {TransparentUpgradeableProxy} from "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
