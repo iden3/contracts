3ebb9bd52a7952550d490a475c42f54fb1e7f42b
vbasiuk <volodymyr.basiuck@gmail.com>
2024-07-30 16:02:01 +0300

    add unit test for Privado Main id type with issuer first state

--- test/validators/v3/index.ts
+++ test/validators/v3/index.ts
@@ -264,6 +264,15 @@ const testCases: any[] = [
     setProofExpiration: tenYears,
     sender: "0x0000000000000000000000000000000000000000",
   },
+  {
+    name: "Privado Main id type, Validate Genesis User State. Issuer Claim IdenState is in published onchain. Revocation State is published onchain. BJJ Proof",
+    stateTransitions: [
+      require("../common-data/issuer_from_genesis_state_to_first_transition_privado_main_v3.json"),
+    ],
+    proofJson: require("./data/valid_bjj_user_genesis_privado_main_v3.json"),
+    setProofExpiration: tenYears,
+    sender: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+  },
 ];
 
 function delay(ms: number) {
