57a2d05d57e882499a719fc40c03812c79edd2fa
Andriian Chestnykh <>
2024-09-07 07:53:11 +0100

    WIP

--- contracts/lib/PrimitiveTypeUtils.sol
+++ contracts/lib/PrimitiveTypeUtils.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.20;
 
 import {BytesLib} from "solidity-bytes-utils/contracts/BytesLib.sol";
+import {console} from "hardhat/console.sol";
 
 library PrimitiveTypeUtils {
     /**
@@ -178,25 +179,29 @@ library PrimitiveTypeUtils {
     }
 
     function bytesToUint256Array(bytes memory data) internal pure returns (uint256[] memory) {
-        uint256 arrayLength = (data.length + 31) / 32; // Round up the required length
+        uint256 arrayLength = (data.length + 30) / 31; // Round up for 31-byte slices
         uint256[] memory result = new uint256[](arrayLength);
 
         for (uint256 i = 0; i < arrayLength; i++) {
             uint256 value;
             assembly {
-                // Load 32 bytes, starting from the current index in data
-                value := mload(add(data, add(32, mul(i, 32))))
+            // Load 31 bytes, starting from the current index in data
+                value := mload(add(data, add(32, mul(i, 31))))
             }
 
-            // If we're on the last chunk and the data length isn't a full 32 bytes,
+            // If we're on the last chunk and the data length isn't a full 31 bytes,
             // we need to mask out the extra bytes.
-            if (i == arrayLength - 1 && data.length % 32 != 0) {
-                uint256 mask = (1 << (8 * (data.length % 32))) - 1;
-                value = value & mask;
-            }
+//            if (i == arrayLength - 1 && data.length % 31 != 0) {
+//                uint256 mask = (1 << (8 * (data.length % 31))) - 1;
+//                value = value & mask;
+//                console.log(data.length % 31);
+//                console.log(mask);
+//            }
+            console.log("value", value & uint256(bytes32(hex"ffff000000000000000000000000000000000000000000000000000000000000")));
 
             result[i] = value;
         }
+        console.log(arrayLength);
 
         return result;
     }
