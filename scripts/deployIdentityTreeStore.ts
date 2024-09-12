5cff4ab5641969202b29679cc9f000e9fc254a16
vbasiuk <volodymyr.basiuck@gmail.com>
2024-09-12 21:46:29 +0300

    throw in ITS deploy if state or poseidon not provided

--- scripts/deployIdentityTreeStore.ts
+++ scripts/deployIdentityTreeStore.ts
@@ -4,13 +4,22 @@ import { DeployHelper } from "../helpers/DeployHelper";
   const deployHelper = await DeployHelper.initialize();
 
   const stateContractAddress = process.env.STATE_CONTRACT_ADDRESS || "";
+  if (!stateContractAddress) {
+    throw new Error("STATE_CONTRACT_ADDRESS is not set");
+  }
   const poseidon2ContractAddress = process.env.POSEIDON_2_CONTRACT_ADDRESS || "";
+  if (!poseidon2ContractAddress) { 
+    throw new Error("POSEIDON_2_CONTRACT_ADDRESS is not set");
+  }
   const poseidon3ContractAddress = process.env.POSEIDON_3_CONTRACT_ADDRESS || "";
+  if (poseidon3ContractAddress) {
+    throw new Error("POSEIDON_3_CONTRACT_ADDRESS is not set");
+  }
 
   await deployHelper.deployIdentityTreeStore(
     stateContractAddress,
     poseidon2ContractAddress,
     poseidon3ContractAddress,
-    // "create2",
+    "basic",
   );
 })();
