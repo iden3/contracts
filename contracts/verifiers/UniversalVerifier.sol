cd11a50897edc7f801210be06c804a17334d1296
daveroga <daveroga@gmail.com>
2024-09-17 13:13:28 +0200

    increase minor version

--- contracts/verifiers/UniversalVerifier.sol
+++ contracts/verifiers/UniversalVerifier.sol
@@ -21,7 +21,7 @@ contract UniversalVerifier is
     /**
      * @dev Version of contract
      */
-    string public constant VERSION = "1.0.2";
+    string public constant VERSION = "1.1.0";
 
     /// @dev Event emitted upon submitting a ZKP request
     event ZKPResponseSubmitted(uint64 indexed requestId, address indexed caller);
