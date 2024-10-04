ca628b21cdb2c2beec6716c0e919a5253cb0842e
Andriian Chestnykh <andriian.chestnykh@gmail.com>
2024-10-04 11:02:58 +0100

    Bump and fix versions

--- contracts/AlwaysRevert.sol
+++ contracts/AlwaysRevert.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.0;
+pragma solidity 0.8.27;
 
 /// @title This contract as a dummy implementation for Proxy contract if we need to revert all calls
 // This can be applied to disable all methods of a proxy contract with explicit error message
