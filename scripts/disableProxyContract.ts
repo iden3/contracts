a025a9fdfb21f8e09c35038d81586ab52efdfce9
Andriian Chestnykh <andriian.chestnykh@gmail.com>
2024-10-07 18:08:45 +0100

    Hotfix and disable old Universal Verifiers

--- scripts/disableProxyContract.ts
+++ scripts/disableProxyContract.ts
@@ -1,26 +1,30 @@
-import { getConfig } from "../helpers/helperUtils";
-import { ethers, upgrades } from "hardhat";
+import hre, { ethers, upgrades } from "hardhat";
 import { expect } from "chai";
 
-const config = getConfig();
-
 // Get proper contract address and name
-const contractAddress = "<put contract address here>";
+const contractAddress = "<put-your-contract-address>";
 
 if (!ethers.isAddress(contractAddress)) {
   throw new Error("Proxy contract address is not set");
 }
 
 async function main() {
+  // Put proper contract name here
+  const contractName = "<put-your-contract-name>";
+  const contract = await ethers.getContractAt(contractName, contractAddress);
+
   const alwaysRevertFactory = await ethers.getContractFactory("AlwaysRevert");
-  await upgrades.upgradeProxy(config.stateContractAddress, alwaysRevertFactory, {
+  const c = await upgrades.upgradeProxy(contract, alwaysRevertFactory, {
     unsafeSkipStorageCheck: true,
   });
+  await c.waitForDeployment();
 
-  // Put proper contract name here
-  // const contractName = "<pub contract name here>";
-  // const contract = await ethers.getContractAt(contractName, contractAddress);
-  // await expect(contract.getDefaultIdType()).to.be.revertedWith("The contract is disabled");
+  await expect(contract.someFunction()).to.be.revertedWith("The contract is disabled");
+
+  const network = hre.network.name;
+  console.log(
+    `The contract ${contractName} at ${contractAddress} on network ${network} is disabled`,
+  );
 }
 
 main()
