a1de5fa285573a8847492ff797a59fbafc4b23b8
daveroga <daveroga@gmail.com>
2024-10-07 20:30:00 +0200

    review changes

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,11 +1,11 @@
 // HARDHAT network Oracle signing address
 import { ethers } from "hardhat";
 
-const oracleSAHardhat = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+const ORACLE_SIGNING_ADDRESS_HARDHAT = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 // TEST networks Oracle signing address (For now we will use in testnets the same production signing address)
-const oracleSATest = "0xf0Ae6D287aF14f180E1FAfe3D2CB62537D7b1A82"; //"0x3e1cFE1b83E7C1CdB0c9558236c1f6C7B203C34e";
+export const ORACLE_SIGNING_ADDRESS_TEST = "0x3e1cFE1b83E7C1CdB0c9558236c1f6C7B203C34e";
 // PRODUCTION networks Oracle signing address
-const oracleSAProd = "0xf0Ae6D287aF14f180E1FAfe3D2CB62537D7b1A82";
+export const ORACLE_SIGNING_ADDRESS_PRODUCTION = "0xf0Ae6D287aF14f180E1FAfe3D2CB62537D7b1A82";
 
 type ChainIdInfo = {
   idType: string;
@@ -14,17 +14,61 @@ type ChainIdInfo = {
 };
 
 export const chainIdInfoMap: Map<number, ChainIdInfo> = new Map()
-  .set(31337, { idType: "0x0212", networkType: "test", oracleSigningAddress: oracleSAHardhat }) // hardhat
-  .set(1101, { idType: "0x0214", networkType: "main", oracleSigningAddress: oracleSAProd }) // polygon zkevm
-  .set(2442, { idType: "0x0215", networkType: "test", oracleSigningAddress: oracleSATest }) // polygon cardona
-  .set(137, { idType: "0x0211", networkType: "main", oracleSigningAddress: oracleSAProd }) // polygon main
-  .set(80001, { idType: "0x0212", networkType: "test", oracleSigningAddress: oracleSATest }) // polygon mumbai
-  .set(80002, { idType: "0x0213", networkType: "test", oracleSigningAddress: oracleSATest }) // polygon amoy
-  .set(11155111, { idType: "0x0223", networkType: "test", oracleSigningAddress: oracleSATest }) // ethereum sepolia
-  .set(21000, { idType: "0x01A1", networkType: "main", oracleSigningAddress: oracleSAProd }) // privado-main
-  .set(21001, { idType: "0x01A2", networkType: "test", oracleSigningAddress: oracleSATest }) // privado-test
-  .set(59144, { idType: "0x0149", networkType: "main", oracleSigningAddress: oracleSAProd }) // linea-main
-  .set(59141, { idType: "0x0148", networkType: "test", oracleSigningAddress: oracleSATest }); // linea-sepolia
+  .set(31337, {
+    idType: "0x0212",
+    networkType: "test",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_HARDHAT,
+  }) // hardhat
+  .set(1101, {
+    idType: "0x0214",
+    networkType: "main",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // polygon zkevm
+  .set(2442, {
+    idType: "0x0215",
+    networkType: "test",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // polygon cardona
+  .set(137, {
+    idType: "0x0211",
+    networkType: "main",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // polygon main
+  .set(80001, {
+    idType: "0x0212",
+    networkType: "test",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // polygon mumbai
+  .set(80002, {
+    idType: "0x0213",
+    networkType: "test",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // polygon amoy
+  .set(11155111, {
+    idType: "0x0223",
+    networkType: "test",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // ethereum sepolia
+  .set(21000, {
+    idType: "0x01A1",
+    networkType: "main",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // privado-main
+  .set(21001, {
+    idType: "0x01A2",
+    networkType: "test",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // privado-test
+  .set(59144, {
+    idType: "0x0149",
+    networkType: "main",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }) // linea-main
+  .set(59141, {
+    idType: "0x0148",
+    networkType: "test",
+    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
+  }); // linea-sepolia
 
 export const CONTRACT_NAMES = Object.freeze({
   UNIVERSAL_VERIFIER: "UniversalVerifier",
--- scripts/setOracleSigningAddress.ts
+++ scripts/setOracleSigningAddress.ts
@@ -1,9 +1,9 @@
 import { ethers } from "hardhat";
 import { getConfig } from "../helpers/helperUtils";
-import { CONTRACT_NAMES } from "../helpers/constants";
+import { CONTRACT_NAMES, ORACLE_SA_PROD } from "../helpers/constants";
 
 async function main() {
-  const oracleSigningAddress = "0xf0Ae6D287aF14f180E1FAfe3D2CB62537D7b1A82"; // production signing address
+  const oracleSigningAddress = ORACLE_SA_PROD; // production signing address
 
   const config = getConfig();
   if (!ethers.isAddress(config.stateContractAddress)) {
@@ -20,7 +20,9 @@ async function main() {
   );
   const tx = await crossChainProofValidator.setOracleSigningAddress(oracleSigningAddress);
 
-  console.log(`Oracle signing address set to ${oracleSigningAddress} in tx ${tx.hash}`);
+  console.log(
+    `Tx ${tx.hash} set oracle signing address to ${oracleSigningAddress} in CrossChainProofValidator contract ${await crossChainProofValidator.getAddress()}`,
+  );
 }
 
 main()
