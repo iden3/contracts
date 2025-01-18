9f791c688ba906a9f34930169ed64e724ff69d7d
Andriian Chestnykh <andriian.chestnykh@gmail.com>
2025-01-18 17:55:22 +0000

    Add more linked multi query tests

--- contracts/validators/LinkedMultiQueryValidator.sol
+++ contracts/validators/LinkedMultiQueryValidator.sol
@@ -8,6 +8,11 @@ import {IState} from "../interfaces/IState.sol";
 import {Ownable2StepUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
+error WrongCircuitID(string circuitID);
+error InvalidQueryHash(uint256 expectedQueryHash, uint256 actualQueryHash);
+error InvalidGroupID(uint256 groupID);
+error TooManyQueries(uint256 operatorCount);
+
 contract LinkedMultiQueryValidator is IRequestValidator, Ownable2StepUpgradeable {
     // This should be limited to the real number of queries in which operator != 0
     struct Query {
@@ -22,26 +27,25 @@ contract LinkedMultiQueryValidator is IRequestValidator, Ownable2StepUpgradeable
     }
 
     /// @dev Main storage structure for the contract
-    /// @custom:storage-location iden3.storage.LinkedMultiQueryValidatorBaseStorage
-    struct LinkedMultiQueryValidatorBaseStorage {
-        //TODO do we need this mapping?
+    /// @custom:storage-location iden3.storage.LinkedMultiQueryValidatorStorage
+    struct LinkedMultiQueryValidatorStorage {
         mapping(string circuitName => IGroth16Verifier) _supportedCircuits;
         string[] _supportedCircuitIds;
     }
 
-    // keccak256(abi.encode(uint256(keccak256("iden3.storage.LinkedMultiQueryValidatorBaseStorage")) - 1))
+    // keccak256(abi.encode(uint256(keccak256("iden3.storage.LinkedMultiQueryValidator")) - 1))
     //  & ~bytes32(uint256(0xff));
-    bytes32 private constant LinkedMultiQueryValidatorBaseStorageLocation =
-        0x2a12018e5edfc1fb8de8bb271d40c512afd1e683a34fc602c9c8e5cfd4529700;
+    bytes32 private constant LinkedMultiQueryValidatorStorageLocation =
+        0x85875fc21d0742149175681df1689e48bce1484a73b475e15e5042650a2d7800;
 
     /// @dev Get the main storage using assembly to ensure specific storage location
-    function _getLinkedMultiQueryValidatorBaseStorage()
+    function _getLinkedMultiQueryValidatorStorage()
         private
         pure
-        returns (LinkedMultiQueryValidatorBaseStorage storage $)
+        returns (LinkedMultiQueryValidatorStorage storage $)
     {
         assembly {
-            $.slot := LinkedMultiQueryValidatorBaseStorageLocation
+            $.slot := LinkedMultiQueryValidatorStorageLocation
         }
     }
 
@@ -55,20 +59,20 @@ contract LinkedMultiQueryValidator is IRequestValidator, Ownable2StepUpgradeable
     struct PubSignals {
         uint256 linkID;
         uint256 merklized;
-        uint256[] operatorOutput;
-        uint256[] circuitQueryHash;
+        uint256[10] operatorOutput;
+        uint256[10] circuitQueryHash;
     }
 
     string public constant VERSION = "1.0.0-beta";
     string internal constant CIRCUIT_ID = "linkedMultiQuery10";
-    uint256 internal constant QUERIES_NUMBER = 10;
+    uint256 internal constant QUERIES_COUNT = 10;
 
     function version() external view override returns (string memory) {
         return VERSION;
     }
 
     function initialize(address _groth16VerifierContractAddr, address owner) public initializer {
-        LinkedMultiQueryValidatorBaseStorage storage $ = _getLinkedMultiQueryValidatorBaseStorage();
+        LinkedMultiQueryValidatorStorage storage $ = _getLinkedMultiQueryValidatorStorage();
         $._supportedCircuits[CIRCUIT_ID] = IGroth16Verifier(_groth16VerifierContractAddr);
         $._supportedCircuitIds.push(CIRCUIT_ID);
     }
@@ -79,32 +83,27 @@ contract LinkedMultiQueryValidator is IRequestValidator, Ownable2StepUpgradeable
         address sender,
         IState state
     ) external view returns (IRequestValidator.ResponseField[] memory) {
-        LinkedMultiQueryValidatorBaseStorage storage $ = _getLinkedMultiQueryValidatorBaseStorage();
+        LinkedMultiQueryValidatorStorage storage $ = _getLinkedMultiQueryValidatorStorage();
 
-        // 0. Parse query
         Query memory query = abi.decode(data, (Query));
-
-        // 1. Parse public signals
         (
             uint256[] memory inputs,
             uint256[2] memory a,
             uint256[2][2] memory b,
             uint256[2] memory c
         ) = abi.decode(proof, (uint256[], uint256[2], uint256[2][2], uint256[2]));
+        PubSignals memory pubSignals = _parsePubSignals(inputs);
 
-        PubSignals memory pubSignals = _parsePubSignals(inputs, QUERIES_NUMBER);
-
-        // 1. Verify circuit query hash for 10
-        // TODO check
-        $._supportedCircuits[CIRCUIT_ID].verify(a, b, c, inputs);
         _checkQueryHash(query, pubSignals);
         _checkGroupId(query.groupID);
 
+        if (keccak256(bytes(query.circuitIds[0])) != keccak256(bytes(CIRCUIT_ID))) {
+            revert WrongCircuitID(query.circuitIds[0]);
+        }
+        $._supportedCircuits[CIRCUIT_ID].verify(a, b, c, inputs);
+
         return _getResponseFields(pubSignals, query);
     }
-
-    error InvalidQueryHash(uint256 expectedQueryHash, uint256 actualQueryHash);
-    error InvalidGroupID(uint256 groupID);
 
     function _checkGroupId(uint256 groupID) internal pure {
         if (groupID == 0) {
@@ -113,7 +112,10 @@ contract LinkedMultiQueryValidator is IRequestValidator, Ownable2StepUpgradeable
     }
 
     function _checkQueryHash(Query memory query, PubSignals memory pubSignals) internal pure {
-        for (uint256 i = 0; i < query.operator.length; i++) {
+        if (query.queryHash.length > QUERIES_COUNT) {
+            revert TooManyQueries(query.queryHash.length);
+        }
+        for (uint256 i = 0; i < query.queryHash.length; i++) {
             if (query.queryHash[i] != pubSignals.circuitQueryHash[i]) {
                 revert InvalidQueryHash(query.queryHash[i], pubSignals.circuitQueryHash[i]);
             }
@@ -121,21 +123,22 @@ contract LinkedMultiQueryValidator is IRequestValidator, Ownable2StepUpgradeable
     }
 
     function _parsePubSignals(
-        uint256[] memory inputs,
-        uint256 queriesQty
+        uint256[] memory inputs
     ) internal pure returns (PubSignals memory) {
-        PubSignals memory pubSignals = PubSignals(
-            0,
-            0,
-            new uint256[](queriesQty),
-            new uint256[](queriesQty)
-        );
+        uint256[QUERIES_COUNT] memory opsOutput;
+        uint256[QUERIES_COUNT] memory queryHashes;
+        PubSignals memory pubSignals = PubSignals({
+            linkID: 0,
+            merklized: 0,
+            operatorOutput: opsOutput,
+            circuitQueryHash: queryHashes
+        });
 
         pubSignals.linkID = inputs[0];
         pubSignals.merklized = inputs[1];
-        for (uint256 i = 0; i < queriesQty; i++) {
+        for (uint256 i = 0; i < QUERIES_COUNT; i++) {
             pubSignals.operatorOutput[i] = inputs[2 + i];
-            pubSignals.circuitQueryHash[i] = inputs[2 + queriesQty + i];
+            pubSignals.circuitQueryHash[i] = inputs[2 + QUERIES_COUNT + i];
         }
         return pubSignals;
     }
--- test/validators/linked-multi-query/linked-multi-query.test.ts
+++ test/validators/linked-multi-query/linked-multi-query.test.ts
@@ -8,6 +8,60 @@ describe("Test linkedMultiQuery10.circom", function () {
   let validator;
   let signer;
 
+  const linkId = "1";
+  const merklized = "1";
+  const operator1 = "1";
+  const operator2 = "16";
+  const operatorOutput1 = "0";
+  const operatorOutput2 = "777";
+  const queryHash1 = "100";
+  const queryHash2 = "200";
+
+  const proofForOneQuery = [linkId, merklized]
+    .concat([operatorOutput1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    .concat([queryHash1, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const proofForTwoQueries = [linkId, merklized]
+    .concat([operatorOutput1, operatorOutput2, 0, 0, 0, 0, 0, 0, 0, 0])
+    .concat([queryHash1, queryHash2, 0, 0, 0, 0, 0, 0, 0, 0]);
+
+  const dummyZKProof = [
+    ["0", "0"],
+    [
+      ["0", "0"],
+      ["0", "0"],
+    ],
+    ["0", "0"],
+  ];
+
+  const oneQuery = {
+    claimPathKey: [0, 0],
+    operator: [operator1],
+    slotIndex: [0, 0],
+    value: [
+      [0, 0],
+      [0, 0],
+    ],
+    queryHash: [queryHash1],
+    circuitIds: ["someWrongCircuitId"],
+    groupID: 1,
+    verifierID: 1,
+  };
+
+  const twoQueries = {
+    claimPathKey: [0, 0],
+    operator: [operator1, operator2],
+    slotIndex: [0, 0],
+    value: [
+      [0, 0],
+      [0, 0],
+    ],
+    queryHash: [queryHash1, queryHash2],
+    circuitIds: ["linkedMultiQuery10"],
+    groupID: 1,
+    verifierID: 1,
+  };
+
   async function deployContractsFixture() {
     [signer] = await ethers.getSigners();
     const groth16Verifier = await ethers.deployContract("Groth16VerifierValidatorStub");
@@ -20,47 +74,9 @@ describe("Test linkedMultiQuery10.circom", function () {
     ({ validator } = await loadFixture(deployContractsFixture));
   });
 
-  it("should work", async function () {
-    const linkId = "1";
-    const merklized = "1";
-    const operator1 = "1";
-    const operator2 = "16";
-    const operatorOutput1 = "0";
-    const operatorOutput2 = "777";
-    const queryHash1 = "100";
-    const queryHash2 = "200";
-
-    const inputs = [linkId, merklized]
-      .concat([operatorOutput1, operatorOutput2])
-      .concat(Array(8).fill("0"))
-      .concat([queryHash1, queryHash2])
-      .concat(Array(8).fill("0"));
-
-    const proof = packZKProof(
-      inputs,
-      ["0", "0"],
-      [
-        ["0", "0"],
-        ["0", "0"],
-      ],
-      ["0", "0"],
-    );
-
-    const query = {
-      claimPathKey: [0, 0],
-      operator: [operator1, operator2],
-      slotIndex: [0, 0],
-      value: [
-        [0, 0],
-        [0, 0],
-      ],
-      queryHash: [queryHash1, queryHash2],
-      circuitIds: [""],
-      groupID: 1,
-      verifierID: 1,
-    };
-
-    const data = packLinkedMultiQueryValidatorParams(query);
+  it("Should verify", async function () {
+    const proof = packZKProof(proofForTwoQueries, ...dummyZKProof);
+    const data = packLinkedMultiQueryValidatorParams(twoQueries);
     const stateAddress = ethers.ZeroAddress;
 
     const result = await validator.verify(proof, data, signer.address, stateAddress);
@@ -72,15 +88,43 @@ describe("Test linkedMultiQuery10.circom", function () {
     // have more than one operator output
   });
 
-  // Merge main dev branch into this branch
+  it("Should throw if circuitId is not linkedMultiQuery10", async function () {
+    const proof = packZKProof(proofForOneQuery, ...dummyZKProof);
+    const data = packLinkedMultiQueryValidatorParams(oneQuery);
+    const stateAddress = ethers.ZeroAddress;
 
-  // CircuitID should be exactly linkedMultiQuery10
+    await expect(validator.verify(proof, data, signer.address, stateAddress))
+      .to.be.revertedWithCustomError(validator, "WrongCircuitID")
+      .withArgs("someWrongCircuitId");
+  });
 
-  // Validate query, all the lengths should be equal and no longer than 10
+  it("More than 10 queries should throw", async function () {
+    const proof = packZKProof(proofForOneQuery, ...dummyZKProof);
+    const data = packLinkedMultiQueryValidatorParams({
+      ...oneQuery,
+      queryHash: Array(11).fill(queryHash1),
+    });
+    const stateAddress = ethers.ZeroAddress;
+
+    await expect(validator.verify(proof, data, signer.address, stateAddress))
+      .to.be.revertedWithCustomError(validator, "TooManyQueries")
+      .withArgs(11);
+  });
+
+  it("Should throw if wrong query hash", async function () {
+    const proof = packZKProof(proofForOneQuery, ...dummyZKProof);
+    const data = packLinkedMultiQueryValidatorParams({
+      ...oneQuery,
+      queryHash: [queryHash2],
+    });
+    const stateAddress = ethers.ZeroAddress;
+
+    await expect(validator.verify(proof, data, signer.address, stateAddress))
+      .to.be.revertedWithCustomError(validator, "InvalidQueryHash")
+      .withArgs(queryHash2, queryHash1);
+  });
+
   // getRequestParams should return the correct values
-
   // Maybe: make programmable groth16 verifier stub and throw error
-  // Should throw if wrong query hash
   // Should throw if groupID is 0
 });
-
