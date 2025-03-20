b70073379dffa06a8e73572d7bf7b29cddace6a6
Andriian Chestnykh <andriian.chestnykh@gmail.com>
2025-03-20 17:14:39 +0100

    Reduce optimizer runs to shrink code size

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -47,7 +47,7 @@ const config: HardhatUserConfig = {
         settings: {
           optimizer: {
             enabled: true,
-            runs: 200,
+            runs: 100,
           },
         },
       },
