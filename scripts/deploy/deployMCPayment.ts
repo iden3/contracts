1ee0549c9fb0af66838772bac22693a0c05378fd
Andriian Chestnykh <AndriianChestnykh@users.noreply.github.com>
2025-03-28 14:01:44 +0000

    Hotfix/constant proxy bytecode (#356)
    * Deploy with constant Proxy bytecode
    
    * Deploy MCPayment with constant Proxy bytecode

--- scripts/deploy/deployMCPayment.ts
+++ scripts/deploy/deployMCPayment.ts
@@ -1,6 +1,9 @@
 import { DeployHelper } from "../../helpers/DeployHelper";
-import { getConfig, verifyContract } from "../../helpers/helperUtils";
+import { getChainId, getConfig, verifyContract } from "../../helpers/helperUtils";
 import { contractsInfo } from "../../helpers/constants";
+import path from "path";
+import hre from "hardhat";
+import fs from "fs";
 
 async function main() {
   const config = getConfig();
@@ -8,10 +11,21 @@ async function main() {
     config.deployStrategy == "create2" ? "create2" : "basic";
 
   const deployHelper = await DeployHelper.initialize(null, true);
-
   const { mcPayment } = await deployHelper.deployMCPayment(10, deployStrategy);
-
   await verifyContract(await mcPayment.getAddress(), contractsInfo.MC_PAYMENT.verificationOpts);
+
+  const chainId = await getChainId();
+  const networkName = hre.network.name;
+  const pathOutputJson = path.join(
+    __dirname,
+    `../deployments_output/deploy_mc_payment_output_${chainId}_${networkName}.json`,
+  );
+  const outputJson = {
+    mcPayment: await mcPayment.getAddress(),
+    network: networkName,
+    chainId: chainId,
+  };
+  fs.writeFileSync(pathOutputJson, JSON.stringify(outputJson, null, 1));
 }
 
 main()
