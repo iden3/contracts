e026a4425a8a012ddd96d5a315e7a00cc82be7f9
daveroga <daveroga@gmail.com>
2025-06-12 10:28:12 +0200

    update deploy libraries

--- scripts/deploy/deployLibraries.ts
+++ scripts/deploy/deployLibraries.ts
@@ -2,47 +2,68 @@ import fs from "fs";
 import path from "path";
 import { getChainId, getConfig, verifyContract } from "../../helpers/helperUtils";
 import { deployPoseidons } from "../../helpers/PoseidonDeployHelper";
-import { DeployHelper } from "../../helpers/DeployHelper";
-import hre from "hardhat";
+import hre, { ethers, ignition } from "hardhat";
 import { contractsInfo } from "../../helpers/constants";
+import {
+  Poseidon1Module,
+  Poseidon2Module,
+  Poseidon3Module,
+  Poseidon4Module,
+  SmtLibModule,
+} from "../../ignition";
 
 async function main() {
   const config = getConfig();
   const deployStrategy: "basic" | "create2" =
     config.deployStrategy == "create2" ? "create2" : "basic";
 
-  const deployHelper = await DeployHelper.initialize(null, true);
-
-  const [poseidon1Elements, poseidon2Elements, poseidon3Elements, poseidon4Elements] =
-    await deployPoseidons([1, 2, 3, 4], deployStrategy);
-
-  const smtLib = await deployHelper.deploySmtLib(
-    await poseidon2Elements.getAddress(),
-    await poseidon3Elements.getAddress(),
-    contractsInfo.SMT_LIB.name,
-    deployStrategy,
-  );
-
-  await verifyContract(await smtLib.getAddress(), contractsInfo.SMT_LIB.verificationOpts);
-
-  const chainId = await getChainId();
+  const [signer] = await ethers.getSigners();
   const networkName = hre.network.name;
-  const outputJson = {
-    poseidon1: await poseidon1Elements.getAddress(),
-    poseidon2: await poseidon2Elements.getAddress(),
-    poseidon3: await poseidon3Elements.getAddress(),
-    poseidon4: await poseidon4Elements.getAddress(),
-    smtLib: await smtLib.getAddress(),
-    network: networkName,
-    chainId,
-    deployStrategy,
-  };
+  const paramsPath = path.join(__dirname, `../../ignition/modules/params/${networkName}.json`);
+  const parameters = JSON.parse(fs.readFileSync(paramsPath).toString());
 
-  const pathOutputJson = path.join(
-    __dirname,
-    `../deployments_output/deploy_libraries_output_${chainId}_${networkName}.json`,
-  );
-  fs.writeFileSync(pathOutputJson, JSON.stringify(outputJson, null, 1));
+  const contracts = [
+    {
+      module: Poseidon1Module,
+      name: contractsInfo.POSEIDON_1.name,
+    },
+    {
+      module: Poseidon2Module,
+      name: contractsInfo.POSEIDON_2.name,
+    },
+    {
+      module: Poseidon3Module,
+      name: contractsInfo.POSEIDON_3.name,
+    },
+    {
+      module: Poseidon4Module,
+      name: contractsInfo.POSEIDON_4.name,
+    },
+    {
+      module: SmtLibModule,
+      name: contractsInfo.SMT_LIB.name,
+      verificationOpts: contractsInfo.SMT_LIB.verificationOpts,
+    },
+  ];
+
+  for (const contract of contracts) {
+    const deployment = await ignition.deploy(contract.module, {
+      strategy: deployStrategy,
+      defaultSender: await signer.getAddress(),
+      parameters: parameters,
+    });
+
+    console.log(
+      `${contract.name} deployed to: ${await deployment[Object.keys(deployment)[0]].target}`,
+    );
+
+    if (contract.verificationOpts) {
+      await verifyContract(
+        await deployment[Object.keys(deployment)[0]].getAddress(),
+        contract.verificationOpts,
+      );
+    }
+  }
 }
 
 main()
