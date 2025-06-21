eb0994325f2a610c849721dd354472dffa95a47e
Andriian Chestnykh <andriian.chestnykh@gmail.com>
2025-06-21 22:42:48 +0200

    Add ingition modules for deploying all the contracts

---
+++ ignition/modules/deployEverythingBasicModel/deployEverything.ts
@@ -0,0 +1,61 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import UniversalVerifierModule from "./universalVerifier";
+import CredentialAtomicQueryMTPV2ValidatorModule from "./credentialAtomicQueryMTPV2Validator";
+import CredentialAtomicQuerySigV2ValidatorModule from "./credentialAtomicQuerySigV2Validator";
+import CredentialAtomicQueryV3ValidatorModule from "./credentialAtomicQueryV3Validator";
+import LinkedMultiQueryValidatorModule from "./linkedMultiQueryValidator";
+import EthIdentityValidatorModule from "./ethIdentityValidator";
+import AuthV2ValidatorModule from "./authV2Validator";
+
+const Everything = buildModule("Create2AddressAnchorModule", (m) => {
+  const { universalVerifier } = m.useModule(UniversalVerifierModule);
+  const { credentialAtomicQueryMTPV2Validator } = m.useModule(
+    CredentialAtomicQueryMTPV2ValidatorModule,
+  );
+  const { credentialAtomicQuerySigV2Validator } = m.useModule(
+    CredentialAtomicQuerySigV2ValidatorModule,
+  );
+  const { credentialAtomicQueryV3Validator } = m.useModule(CredentialAtomicQueryV3ValidatorModule);
+  const { linkedMultiQueryValidator } = m.useModule(LinkedMultiQueryValidatorModule);
+  const { ethIdentityValidator } = m.useModule(EthIdentityValidatorModule);
+  const { authV2Validator } = m.useModule(AuthV2ValidatorModule);
+
+  const contractOwner = m.getAccount(0);
+
+  m.call(universalVerifier, "addValidatorToWhitelist", [credentialAtomicQueryMTPV2Validator], {
+    id: "addValidatorToWhitelist_credentialAtomicQueryMTPV2Validator",
+    from: contractOwner,
+  });
+  m.call(universalVerifier, "addValidatorToWhitelist", [credentialAtomicQuerySigV2Validator], {
+    id: "addValidatorToWhitelist_credentialAtomicQuerySigV2Validator",
+    from: contractOwner,
+  });
+  m.call(universalVerifier, "addValidatorToWhitelist", [credentialAtomicQueryV3Validator], {
+    id: "addValidatorToWhitelist_credentialAtomicQueryV3Validator",
+    from: contractOwner,
+  });
+  m.call(universalVerifier, "addValidatorToWhitelist", [linkedMultiQueryValidator], {
+    id: "addValidatorToWhitelist_linkedMultiQueryValidator",
+    from: contractOwner,
+  });
+  m.call(
+    universalVerifier,
+    "setAuthMethod",
+    [{ authMethod: "ethIdentity", validator: ethIdentityValidator, params: "0x" }],
+    {
+      id: "setAuthMethod_ethIdentityValidator",
+      from: contractOwner,
+    },
+  );
+  m.call(
+    universalVerifier,
+    "setAuthMethod",
+    [{ authMethod: "authV2", validator: authV2Validator, params: "0x" }],
+    {
+      id: "setAuthMethod_authV2Validator",
+      from: contractOwner,
+    },
+  );
+});
+
+export default Everything;
---
+++ ignition/modules/deployEverythingBasicModel/universalVerifier.ts
@@ -0,0 +1,59 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import {
+  contractsInfo,
+  TRANSPARENT_UPGRADEABLE_PROXY_ABI,
+  TRANSPARENT_UPGRADEABLE_PROXY_BYTECODE,
+} from "../../../helpers/constants";
+import StateProxyModule from "./state";
+
+const VerifierLibModule = buildModule("VerifierLibModule", (m) => {
+  const verifierLib = m.contract(contractsInfo.VERIFIER_LIB.name);
+  return { verifierLib };
+});
+
+const UniversalVerifierImplementationModule = buildModule(
+  "UniversalVerifierImplementationModule",
+  (m) => {
+    const { verifierLib } = m.useModule(VerifierLibModule);
+
+    const implementation = m.contract("UniversalVerifier", [], {
+      libraries: {
+        VerifierLib: verifierLib,
+      },
+    });
+
+    return { implementation };
+  },
+);
+
+const UniversalVerifierProxyModule = buildModule("UniversalVerifierProxyModule", (m) => {
+  const { implementation } = m.useModule(UniversalVerifierImplementationModule);
+  const state = m.useModule(StateProxyModule).proxy;
+
+  const proxyAdminOwner = m.getAccount(0);
+  const contractOwner = m.getAccount(0);
+
+  const initializeData = m.encodeFunctionCall(implementation, "initialize", [state, contractOwner]);
+
+  const proxy = m.contract(
+    "TransparentUpgradeableProxy",
+    {
+      abi: TRANSPARENT_UPGRADEABLE_PROXY_ABI,
+      contractName: "TransparentUpgradeableProxy",
+      bytecode: TRANSPARENT_UPGRADEABLE_PROXY_BYTECODE,
+      sourceName: "",
+      linkReferences: {},
+    },
+    [implementation, proxyAdminOwner, initializeData],
+  );
+
+  return { proxy };
+});
+
+const UniversalVerifierModule = buildModule("UniversalVerifierModule", (m) => {
+  const { proxy } = m.useModule(UniversalVerifierProxyModule);
+  const universalVerifier = m.contractAt(contractsInfo.UNIVERSAL_VERIFIER.name, proxy);
+  return { universalVerifier };
+});
+
+export default UniversalVerifierModule;
