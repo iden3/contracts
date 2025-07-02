0e597723b7e59d196f0ee42475dbd6345ecf0aa1
Andriian Chestnykh <andriian.chestnykh@gmail.com>
2025-07-02 13:44:56 +0200

    Small fix

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -87,17 +87,7 @@ export const chainIdInfoMap: Map<number, ChainIdInfo> = new Map()
     idType: "0x0148",
     networkType: "test",
     oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
-  }) // linea-sepolia
-  .set(1819540093, {
-    idType: "0x0113", // like on polygon amoy
-    networkType: "test",
-    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
-  }) // tryagain-bali-03
-  .set(1313161555, {
-    idType: "0x0113", // like on polygon amoy
-    networkType: "test",
-    oracleSigningAddress: ORACLE_SIGNING_ADDRESS_PRODUCTION,
-  }); // aurora-testnet
+  }); // linea-sepolia
 
 export const networks = Object.freeze({
   PRIVADO_TESTNET: { name: "Privado Testnet", chainId: 21001 },
