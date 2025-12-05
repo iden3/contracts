a5721e96136a69c1ccef5f740ff14a951049f74c
Jim Zhang <jim.zhang@kaleido.io>
2025-12-05 13:30:37 -0500

    change IHasher to an interface
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- contracts/interfaces/IHasher.sol
+++ contracts/interfaces/IHasher.sol
@@ -4,18 +4,18 @@ pragma solidity 0.8.27;
 /**
  * @dev IHasher. Interface for generating hashes. Specifically used for Merkle Tree hashing.
  */
-abstract contract IHasher {
+interface IHasher {
     /**
      * @dev hash2. hashes two uint256 parameters and returns the resulting hash as uint256.
      * @param params The parameters array of size 2 to be hashed.
      * @return The resulting hash as uint256.
      */
-    function hash2(uint256[2] memory params) external pure virtual returns (uint256);
+    function hash2(uint256[2] memory params) external pure returns (uint256);
 
     /**
      * @dev hash3. hashes three uint256 parameters and returns the resulting hash as uint256.
      * @param params The parameters array of size 3 to be hashed.
      * @return The resulting hash as uint256.
      */
-    function hash3(uint256[3] memory params) external pure virtual returns (uint256);
+    function hash3(uint256[3] memory params) external pure returns (uint256);
 }
