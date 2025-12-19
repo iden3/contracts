4da032699069cb1d3fce4903bc5932bcae0403f4
David Rodriguez <70919589+daveroga@users.noreply.github.com>
2025-12-19 15:48:57 +0000

    Check issuer part for transferring (#407)
    * check issuer part for transferring
    
    * increase version
    
    * add test

--- contracts/payment/MCPayment.sol
+++ contracts/payment/MCPayment.sol
@@ -18,7 +18,7 @@ contract MCPayment is Ownable2StepUpgradeable, EIP712Upgradeable, ReentrancyGuar
     /**
      * @dev Version of contract
      */
-    string public constant VERSION = "1.0.5";
+    string public constant VERSION = "1.0.6";
 
     /**
      * @dev Version of EIP 712 domain
@@ -455,7 +455,9 @@ contract MCPayment is Ownable2StepUpgradeable, EIP712Upgradeable, ReentrancyGuar
         uint8 ownerPercentage = getIssuerOwnerPercentage(paymentData.recipient);
         uint256 ownerPart = (paymentData.amount * ownerPercentage) / 100;
         uint256 issuerPart = paymentData.amount - ownerPart;
-        token.transfer(paymentData.recipient, issuerPart);
+        if (issuerPart > 0) {
+            token.transfer(paymentData.recipient, issuerPart);
+        }
         emit Payment(signer, paymentData.nonce);
         bytes32 paymentId = keccak256(abi.encode(signer, paymentData.nonce));
         $.isPaid[paymentId] = true;
